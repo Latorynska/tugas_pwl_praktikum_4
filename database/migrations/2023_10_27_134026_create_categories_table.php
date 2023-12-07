447cde60580dda080408a6e40477561a307ea459
Latorynska <paksiringkang225@gmail.com>
2023-12-08 05:26:42 +0700

    update : tugas bab4 modul (rework)

---
+++ database/migrations/2023_10_27_134026_create_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->String('category');
+            $table->timestamps();
+        });
+
+        Schema::table('books', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('category_id')->after('quantity')->nullable();
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void{
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
+        Schema::dropIfExists('categories');
+    }
+
+};
